4514022bd568fd2089436da7a40fbdaedff1b0a2
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-05-23 19:00:29 +0900

    Update C and Ruby example

--- example/mconfig.c
+++ example/mconfig.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <mruby.h>
 #include <mruby/variable.h>
 #include <mruby/hash.h>
@@ -5,6 +6,11 @@
 static mrb_value get_config_value(mrb_state *mrb, char *key)
 {
   return mrb_funcall(mrb, mrb_top_self(mrb), "get_config", 1, mrb_str_new_cstr(mrb, key));
+}
+
+static mrb_value get_sub_config_value(mrb_state *mrb, char *tag, char *key)
+{
+  return mrb_funcall(mrb, mrb_top_self(mrb), "get_sub_config", 2, mrb_str_new_cstr(mrb, tag), mrb_str_new_cstr(mrb, key));
 }
 
 int main() {
@@ -21,6 +27,25 @@ int main() {
   mrb_value extend_status   = get_config_value(mrb, "ExtendedStatus");
   mrb_value user            = get_config_value(mrb, "User");
   mrb_value group           = get_config_value(mrb, "Group");
+
+  mrb_value tag1_file       = get_sub_config_value(mrb, "tag1", "Files");
+  mrb_value tag1_limit      = get_sub_config_value(mrb, "tag1", "AccessLimit");
+  mrb_value tag2_file       = get_sub_config_value(mrb, "tag2", "Files");
+  mrb_value tag2_limit      = get_sub_config_value(mrb, "tag2", "AccessLimit");
+
+  printf("=== global configuration ===\n");
+  mrb_p(mrb, listen_port);
+  mrb_p(mrb, document_root);
+  mrb_p(mrb, extend_status);
+  mrb_p(mrb, user);
+  mrb_p(mrb, group);
+  printf("\n");
+
+  printf("=== tag configuration ===\n");
+  mrb_p(mrb, tag1_file);
+  mrb_p(mrb, tag1_limit);
+  mrb_p(mrb, tag2_file);
+  mrb_p(mrb, tag2_limit);
 
   mrb_close(mrb);
 
