3fc4b2164c67c1fc7b291449142cb5e0ff03ad13
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-05-23 11:58:45 +0900

    Update C exmaple

--- example/mconfig.c
+++ example/mconfig.c
@@ -7,19 +7,6 @@ static mrb_value get_config_value(mrb_state *mrb, char *key)
   return mrb_funcall(mrb, mrb_top_self(mrb), "get_config", 1, mrb_str_new_cstr(mrb, key));
 }
 
-static mrb_value get_config_all_value(mrb_state *mrb)
-{
-  return mrb_funcall(mrb, mrb_top_self(mrb), "get_config", 0);
-}
-
-static void create_config(mrb_state *mrb, char *key, char *val)
-{
-  mrb_value conf;
-  conf = mrb_hash_new(mrb);
-  mrb_hash_set(mrb, conf, mrb_str_new_cstr(mrb, key), mrb_str_new_cstr(mrb, val));
-  mrb_funcall(mrb, mrb_top_self(mrb), "new_config", 1, conf);
-}
-
 int main() {
 
   FILE *fp;
@@ -27,9 +14,6 @@ int main() {
     return 1;
 
   mrb_state* mrb = mrb_open();
-
-  create_config(mrb, "Version", "2");
-
   mrb_load_file(mrb, fp);
 
   mrb_value listen_port     = get_config_value(mrb, "Listen");
@@ -40,7 +24,7 @@ int main() {
 
   mrb_close(mrb);
 
-  // 設定の値を使って色々と処理
+  // Implement code using configuration value in Ruby
 
   return 0;
 }
