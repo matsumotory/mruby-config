1db278fd5850521944bbe2607820382d037d330a
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2013-06-06 19:16:26 +0900

    Add arena_restore

--- src/mrb_config.c
+++ src/mrb_config.c
@@ -16,6 +16,8 @@
 #define GLOBAL_CONFIG_KEY       "$mrb_g_config"
 #define GLOBAL_SUB_CONFIG_KEY   "$mrb_g_sub_config"
 
+#define DONE mrb_gc_arena_restore(mrb, 0);
+
 /*
 ** Kernel#{new_config,get_config,add_config,del_config}
 ** Config#{get,add,del}
@@ -365,16 +367,19 @@ void mrb_mruby_config_gem_init(mrb_state *mrb)
   mrb_define_method(mrb, mrb->kernel_module, "add_sub_config", mrb_config_sub_add,    MRB_ARGS_REQ(2));
   mrb_define_method(mrb, mrb->kernel_module, "del_sub_config", mrb_config_sub_del,    MRB_ARGS_OPT(2));
   mrb_define_method(mrb, mrb->kernel_module, "get_sub_config", mrb_config_sub_get,    MRB_ARGS_OPT(2));
+  DONE;
 
   config = mrb_define_class(mrb, "Config", mrb->object_class);
   mrb_define_class_method(mrb, config, "add", mrb_config_add, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, config, "del", mrb_config_del, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, config, "get", mrb_config_get, MRB_ARGS_OPT(1));
+  DONE;
 
   sub_config = mrb_define_class_under(mrb, config, "Sub", mrb->object_class);
   mrb_define_class_method(mrb, sub_config, "add", mrb_config_sub_add, MRB_ARGS_REQ(2));
   mrb_define_class_method(mrb, sub_config, "del", mrb_config_sub_del, MRB_ARGS_REQ(2));
   mrb_define_class_method(mrb, sub_config, "get", mrb_config_sub_get, MRB_ARGS_OPT(2));
+  DONE;
 }
 
 void mrb_mruby_config_gem_final(mrb_state *mrb)
